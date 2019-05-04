604116c9aaa5176365aef7cb598f590f5a6e1379
xquan <xquan.s@163.com>
2019-05-05 01:01:18 +0800

    modify cmake

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,14 @@
 #include <jni.h>
 #include <string>
 
+#include <rtc_base/ssl_adapter.h>
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_eco_data_m3_net_p2p_test_1jni_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
+
+    rtc::InitializeSSL();
     return env->NewStringUTF(hello.c_str());
 }
